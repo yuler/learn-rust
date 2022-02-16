18711f52020c226d3ff3f54e688f06e3c6d3587d
yuler <is.yuler@gmail.com>
2022-02-16 13:26:31 +0800

    refactor: rename some directory name

--- explore/src/main.rs
+++ explore/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     let data = vec![1, 2, 3, 4];
     let data1 = data;
     println!("sum of data1: {}", sum(data1));
-    println!("data1: {:?}", data1); // error1
-    println!("sum of data: {}", sum(data)); // error2
+    // println!("data1: {:?}", data1); // error1
+    // println!("sum of data: {}", sum(data)); // error2
 }
 
 fn sum(data: Vec<u32>) -> u32 {
